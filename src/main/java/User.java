d273fbcb2c64909b0f2cce3a38d7c0ef794024f2
Tarasov Ser <111812342+tarasapiens@users.noreply.github.com>
2024-01-22 03:48:48 +0300

    Update User.java

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,20 +1,4 @@
-//public class User {
- //   String name;
- //   String nickname;
- //   String email;
- //   int age;
- //   int rating;
- //   boolean isonline;
-
- //   public User(String nametobe, String emailtobe, int  agetobe){
-  //      System.out.println("привет, я создался " + nametobe);
-  //      name = nametobe;
-  //      email = emailtobe;
-  //      age = agetobe;
-  //      nickname = emailtobe;
-  //      rating = 0;;
-   //     isonline = true;;
-
+//
 
 
 
